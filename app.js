c6126344d6ec3ff4f20bf04b2ba409e09202eacd
denikryt <56690968+denikryt@users.noreply.github.com>
2023-01-31 02:27:59 +0200

    new project

---
+++ app.js
@@ -0,0 +1,167 @@
+const TelegramBot = require('node-telegram-bot-api');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const youtube = require('youtube-metadata-from-url');
+const { Keyboard, Key } = require('telegram-keyboard')
+const database = require('./database.js');
+
+const token = '5301226184:AAFeEf2yfBMjjBFEZgVEk9eSFQP4VilACUY';
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', async (message) => {
+  // return
+  const youtubeURL = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)(\/[^&\n\r]+)$/;
+  const myID = 183278535 
+  let user_id = message.chat.id
+  let message_id = message.message_id
+  let user_name = message.chat.username
+  let data = {user_id: user_id, message_id: message_id, user_name: user_name}
+
+  let exists = await database.check_and_create(data) 
+  if (exists){
+    await database.update_last_message(data)
+  }
+  // return
+
+  if(message.chat.id !== 183278535) {
+    // bot.sendMessage(myID, message.text)
+    await database.update_last_message(data)
+    return
+  }
+  
+  // const keyboard = Keyboard.make([
+  //   Key.callback('Button 1', 'action1'),
+  //   Key.callback('Button 2', 'action2'),
+  //   ]).inline()
+
+  const keyboard = Keyboard.make([
+    Key.callbackWithImage('Button 1', 'action1', 'https://i.ytimg.com/vi/mgt-6PPrJzA/hqdefault.jpg')
+    ]).inline()
+
+  bot.sendMessage(183278535, message.text, keyboard)
+  try {
+    await database.update_last_message(data)
+    await database.set_channel_id(data)
+    await database.create_collection(data)
+
+  } catch (error) {
+    console.log(`ERROR WITH DATABASES ${error}`)
+  }
+  return
+});
+
+bot.on('channel_post', async (channelPost) => {
+  // return
+  let message = channelPost;  
+  const myID = 183278535 
+
+  // check if the channelPost comes from the specific channel
+  if(channelPost.chat.id === -1001830200744) {
+
+    const youtubeURL = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)(\/[^&\n\r]+)$/;
+    if(youtubeURL.test(message.text)) {
+      let data = await getYoutubeData(message.text)
+      
+      const keyboard = Keyboard.make([
+        Key.callback('Добавить', 'add'),
+        Key.callback('Изменить', 'edit'),
+        Key.callback('Удалить', 'delete'),
+      ]).inline()
+
+      data['channel_id'] = channelPost.chat.id
+
+      let user = await database.get_user_by_channel_id(data.channel_id)
+
+      sendMessage(user.user_id, data.text, keyboard);
+      await database.update_last_message_by_channel_id(data)
+      await database.new_track(data)
+    }
+  }
+});
+
+bot.on('callback_query', async (callbackQuery) => {
+  const data = callbackQuery.data;
+  const message = callbackQuery.message;
+  const chatId = message.chat.id;
+
+  switch(data){
+      case 'add':
+          return;
+
+      case 'edit':
+          // do something with option 2
+          break;
+
+      case 'delete':
+        break
+      // default:
+          // handle other cases
+  }
+});
+
+///////////////////////////////////////////////////////
+
+function sendMessage(chatId, text, inline_keyboard) {
+  if(inline_keyboard){
+    bot.sendMessage(chatId, text, inline_keyboard)
+  }else{
+    bot.sendMessage(chatId,text);
+    }
+}
+
+async function getYoutubeData(url){
+  const youtubeURL = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)(\/[^&\n\r]+)$/;
+  const youtubePlaylistLink = /^(?:https?:\/\/)?(?:www\.)?youtube\.com\/playlist\?list=([^&]+)$/;
+  let text 
+  let title
+  let thumbnail
+
+    if(youtubeURL.test(url)) {
+      try { 
+        const metadata = await youtube.metadata(url);
+        console.log('METADATA',metadata);
+        title = metadata.title;
+        thumbnail = metadata.thumbnail_url;
+        text = `${title} \n${thumbnail}`;
+        return {text, url, title, thumbnail}
+  
+      } catch (error) {
+        try {
+          const response = await axios.get(url, {
+            headers: {
+              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
+              'Accept-Language': 'en-US,en;q=0.9',
+              'Accept-Encoding': 'gzip, deflate, br',
+              'Connection': 'keep-alive',
+              'Upgrade-Insecure-Requests': '1'
+            }
+          });
+      
+          const $ = cheerio.load(response.data);
+          title = $("#watch7-content > meta:nth-child(2)").attr("content");
+          text = `${title}`
+          return {text, url, title, thumbnail}
+          
+        } catch (error) {
+          sendMessage(userID, "ERROR")
+          console.error(error);
+        }
+      }
+    }
+
+    if(youtubePlaylistLink.test(url)) {
+      try {
+        console.log(await youtube.metadata(url));
+        title = (await youtube.metadata(url)).title;
+        thumbnail = (await youtube.metadata(url)).thumbnail_url;
+        text = `${title} \n${thumbnail}`;
+        return {text, url, title, thumbnail}
+
+      } catch (error) {
+        sendMessage(userID, "ERROR")
+        console.error(error);
+      }
+    } 
+ 
+}
---
+++ database.js
@@ -0,0 +1,107 @@
+const { userSchema, trackSchema } = require('./models.js');
+const mongoose = require('mongoose');
+
+// const uri = 'mongodb+srv://denchik:D3778228545dan@cluster0.ntwfb.mongodb.net/test?retryWrites=true&w=majority'
+
+// const client = new MongoClient(uri);
+
+// const db = client.db('test');
+// const Users = db.collection('users');
+// const SoundTracks = db.collection('soundtracks');
+
+const Users = mongoose.model('users', userSchema);
+
+async function check_and_create(data){
+    let user = await Users.findOne({user_id: data.user_id})
+
+        if(!user){
+            const doc = {
+                user_id: data.user_id,
+                user_name: data.user_name,
+                last_message_id: data.message_id
+            }
+            const user = new Users(doc)
+            await user.save();
+            return false
+            
+        }else{
+            console.log(data.user_id, 'ЮЗЕР УЖЕ ЕСТЬ!')
+            return true 
+        }
+        return
+}
+
+async function update_last_message(data){
+    await Users.findOneAndUpdate(
+        { user_id: data.user_id }, 
+        { $inc: { last_message_id: 1 } },
+        { new: true }
+        );
+        return
+}
+
+async function update_last_message_by_channel_id(data){
+    await Users.findOneAndUpdate(
+        { channel_id: data.channel_id }, 
+        { $inc: { last_message_id: 1 } },
+        { new: true }
+        );
+        return
+}
+
+async function new_track(data){
+    const user = await get_user_by_channel_id(data.channel_id)
+    let collectionName = `${user.user_name}(${user.user_id})`;
+    const soundTracks = mongoose.model(collectionName, trackSchema);
+    const exists = await soundTracks.findOne({ url: data.url });
+
+    if (!exists){
+        const newTrack = new soundTracks({
+            url: data.url,
+            thumbnail: data.thumbnail,
+            title: data.title,
+            message_id: user.last_message_id
+        })
+        await newTrack.save() 
+
+    } else {
+        sendMessage(user_id, 'УЖЕ ЕСТЬ!')
+    }
+    return
+}
+
+async function set_channel_id(data){
+    await Users.findOneAndUpdate(
+        { user_id: data.user_id }, 
+        { $set: { channel_id: -1001830200744 } },
+        { upsert: false, new: true })
+      return
+}
+
+async function create_collection(data){
+    const collection_name = `${data.user_name}(${data.user_id})`;
+        try {
+            if(!mongoose.connection.collections[collection_name]){
+                let Tracks = mongoose.model(collection_name, trackSchema);
+            }
+        } catch (error) {
+            console.log(`An error occurred while creating the collection: ${error}`);
+        }
+        return
+}
+
+async function get_user_by_channel_id(channel_id){
+    const user = await Users.findOne({ channel_id: channel_id });
+    return user
+}
+
+const functions = {
+    check_and_create,
+    update_last_message,
+    update_last_message_by_channel_id,
+    get_user_by_channel_id,
+    new_track,
+    set_channel_id,
+    create_collection
+}
+module.exports = functions;
